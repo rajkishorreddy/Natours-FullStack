57e7aa4a3cae4c75a6ea9bbaa4382343930b41e0
rajkishorreddy <rajakishorbeeravalli@gmail.com>
2021-02-04 23:19:16 +0530

    implimented email-template

---
+++ controllers/viewsController.js
@@ -0,0 +1,80 @@
+const Tour = require('./../models/tourModel');
+const catchAsync = require('./../utils/catchAsync');
+const AppError = require('./../utils/appError');
+const User = require('./../models/usermodel');
+const { findById, findByIdAndUpdate } = require('./../models/tourModel');
+
+exports.getOverview = catchAsync(async (req, res, next) => {
+  //1 get all the tour data from the collection
+  const tours = await Tour.find();
+  //2 we build template
+  //3 render that template using tour data from step 1
+  res.status(200).render('overview', {
+    title: 'All Tours',
+    tours,
+  });
+});
+exports.getTour = catchAsync(async (req, res, next) => {
+  const tour = await Tour.findOne({ slug: req.params.slug }).populate({
+    path: 'reviews',
+    fields: 'review rating user',
+  });
+  if (!tour) {
+    return next(new AppError(`there is no tour with that name`, 404));
+  }
+  res
+    .status(200) //done this using stackover flow to make it work
+    .set(
+      'Content-Security-Policy',
+      "default-src 'self' https://*.mapbox.com ;base-uri 'self';block-all-mixed-content;font-src 'self' https: data:;frame-ancestors 'self';img-src 'self' data:;object-src 'none';script-src https://cdnjs.cloudflare.com https://api.mapbox.com 'self' blob: ;script-src-attr 'none';style-src 'self' https: 'unsafe-inline';upgrade-insecure-requests;"
+    )
+    .render('tour', {
+      title: `${tour.name} Tour`,
+      tour,
+    });
+});
+exports.login = (req, res) => {
+  res
+    .status(200)
+    .set(
+      'Content-Security-Policy',
+      "connect-src 'self' https://cdnjs.cloudflare.com"
+    )
+    .render('login', {
+      title: 'Log into your account',
+    });
+};
+exports.getAccount = (req, res) => {
+  res
+    .status(200)
+    .set(
+      'Content-Security-Policy',
+      "connect-src 'self' https://cdnjs.cloudflare.com"
+    )
+    .render('account', {
+      title: 'yout account',
+    });
+};
+exports.updateUserData = catchAsync(async (req, res, next) => {
+  const updatedUser = await User.findByIdAndUpdate(
+    req.user.id,
+    {
+      name: req.body.name,
+      email: req.body.email,
+    },
+    {
+      new: true,
+      runValidators: true,
+    }
+  );
+  res
+    .status(200)
+    .set(
+      'Content-Security-Policy',
+      "connect-src 'self' https://cdnjs.cloudflare.com"
+    )
+    .render('account', {
+      title: 'your account',
+      user: updatedUser,
+    });
+});
